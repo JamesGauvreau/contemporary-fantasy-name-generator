6412cebcdeaa5bc095295aaa2591f4189c37fe31
cmSalticidae <james.longfrenchname@gmail.com>
2025-06-29 16:15:04 -0700

    additional notes for name generator

---
+++ name-gen.js
@@ -0,0 +1,24 @@
+//  TODO
+//  v1      minimum viable product
+//          - press a button, get some names
+//  v1.x    add diminutives
+//  v1.x    integrate family names
+//  v1.x    fine-tune the odds
+//  v1.x    work with keys
+//  v1.x    Add celestial names, etc.
+//  v2      Build diminutives from roots
+//  v3      Work with subtypes
+
+//  PSEUDO 
+//  Check # of objects in super_array
+//  Roll random number based on # of objects in super_array
+//  Determine gender (Witch || Wizard) by random flip. 
+//  Access a particular object
+//  Check how many names in array (object_name.english_M or object_name.english_F)
+    //  If 1 name, pick it.
+    //  If 2 names, pick the second.
+    //  Else, Roll Math.random()
+        // If 0.5 or lower, pick first option in array (object_name.english_M || object_name.english_F)
+        //  Else, Roll Math.ceil(Math.random() * names in array)
+            //  Use # to pick an option.
+        
